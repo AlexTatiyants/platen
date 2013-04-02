4f463191f9c040b895f10195f06b7f1494d14475
Alex Tatiyants <atatiyan@gmail.com>
2013-04-01 22:35:49 -0700

    iniitial commit
    - added support for WordPress XML RPC API calls (via wordpress.js)
    - added support for Markdown conversions (via marked.js)
    - added starter angular app

---
+++ controllers/editor.js
@@ -0,0 +1,9 @@
+var EditorController = function($scope) {
+
+	$scope.post = {};
+
+	$scope.update = function () {
+		$scope.post.htmlContent = marked($scope.post.rawContent);
+		console.log($scope.post);
+	}
+};
---
+++ main.js
@@ -0,0 +1,13 @@
+// var connection = {
+// };
+
+// var wp = new WordPress(connection.url, connection.username, connection.password);
+// // var blogId = 1;
+// // var postId = 1858;
+// // var object = wp.getPost(blogId, postId);
+
+// // console.log(object.post_content)
+
+// var posts = wp.getPosts(1, {post_type:"post"});
+
+// console.log(posts);
